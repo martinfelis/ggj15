aec107b43925893d07e61c2aa69bfbfac6b613d1
Phaiax <phaiax@invisibletower.de>
2015-01-25 16:12:06 +0100

    release script update

--- distribution/makerelease.sh
+++ distribution/makerelease.sh
@@ -2,7 +2,7 @@
 
 cd distribution
 cd ..
-zip -9 -q -r  prisonbroke.zip . --exclude=.git/\*
+zip -9 -r  prisonbroke.zip . --exclude=.git/\* --exclude=distribution/\*
 mv prisonbroke.zip distribution/prisonbroke.love
 cd distribution
 
@@ -11,11 +11,15 @@ cd distribution
 unzip love-0.9.1-win32.zip.1
 mv love-0.9.1-win32 Prisonbroke
 cat Prisonbroke/love.exe prisonbroke.love > Prisonbroke/Prisonbroke.exe
+rm Prisonbroke/love.exe
 zip -9 -r Prisonbroke_Win32.zip Prisonbroke/
 rm -rf Prisonbroke
 
 unzip love-0.9.1-win64.zip.1
 mv love-0.9.1-win64 Prisonbroke
 cat Prisonbroke/love.exe prisonbroke.love > Prisonbroke/Prisonbroke.exe
+rm Prisonbroke/love.exe
 zip -9 -r Prisonbroke_Win64.zip Prisonbroke/
 rm -rf Prisonbroke
+
+cp prisonbroke.love prisonbroke_source.zip
