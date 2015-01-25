c155d9cbbdcbf0c7bd4bbc0597d0940569144945
fysx <martin@fysx.org>
2015-01-25 11:00:55 +0100

    drawing guards

--- entities/guard.lua
+++ entities/guard.lua
@@ -13,6 +13,10 @@ local function newGuard (x, y, world)
 
 		alerted = false,
 		alertness = 0.,
+		image = "images/guard.png",
+		
+		cycle_phase = 0.,
+		speed = 0.,
 
 		detectortype = "guard",
 		player_alert_start= {},
@@ -25,7 +29,7 @@ local function newGuard (x, y, world)
 	guard.body:setLinearDamping(5)
 	guard.shape2 = love.physics.newCircleShape(20)
 	guard.fixture2 = love.physics.newFixture(guard.guide, guard.shape2)
-
+	guard.image = love.graphics.newImage(guard.image)
 
 	guard.joint = love.physics.newRopeJoint(guard.guide, guard.body, 0,0,0,0, 20, false)
 
@@ -48,8 +52,8 @@ local function newGuard (x, y, world)
 			 -- self.angle = math.atan2(endY-startY, endX-startX)
 		end
 
-		local vel= {}
-		vel.x, vel.y = self.body:getLinearVelocity()
+		local vel = vector (self.body:getLinearVelocity())
+
 		self.angle = math.atan2(vel.y, vel.x)
 
 		-- guard
@@ -110,11 +114,23 @@ local function newGuard (x, y, world)
 			self.alerted = false
 		end
 
+		if self.cycle_phase > math.pi * 2 then
+			self.cycle_phase = self.cycle_phase - math.pi * 2
+		elseif self.cycle_phase < 0 then
+			self.cycle_phase = self.cycle_phase + math.pi * 2
+		end
+
+		if self.speed > 0.1 then
+			self.cycle_phase = self.cycle_phase + dt * self.speed / 30
+		else
+			self.speed = 0.
+			vel.x, vel.y = 0., 0.
+		end
 	end
 
 	function guard:draw()
 		love.graphics.setColor(255,(1. - self.alertness) * 255, 0, 128)
-																		   -- fix löves wrong angle drawing
+		-- fix löves wrong angle drawing
 		love.graphics.arc("fill", self.x, self.y, self.radius, self.angle + self.fov*.5, self.angle - self.fov *.5)
 
 		if self.alerted then
@@ -122,6 +138,54 @@ local function newGuard (x, y, world)
 			love.graphics.setLineWidth (10.)
 			love.graphics.arc("line", self.x, self.y, self.radius + math.sin(love.timer.getTime() * 10) * 10, self.angle + self.fov*.5, self.angle - self.fov *.5)
 		end
+
+		if not self.alerted then
+			love.graphics.setColor (10, 10, 255, 255)
+		end
+		local draw_angle = self.angle + math.pi * 0.5
+		love.graphics.draw(self.image, self.body:getX(), self.body:getY(),draw_angle, 2,2,self.image:getWidth() /2, self.image:getHeight()/2)
+
+		local arm_width = 20
+		local shoulder_width = 38.
+		local phase_mod = math.sin (self.cycle_phase)
+		local arm_length = 20 + 30 * math.abs(phase_mod)
+		local arm_center = vector(arm_width * 0.5 + shoulder_width, phase_mod * 10)
+
+		local foot_width = 20
+		local hip_width = 10.
+		local phase_mod = math.sin (-self.cycle_phase)
+		local foot_length = 30 + 20 * math.abs(phase_mod)
+		local foot_center = vector(foot_width * 0.5 + hip_width, phase_mod * 10)
+
+		love.graphics.push()
+		love.graphics.translate (self.body:getX(), self.body:getY())
+		love.graphics.rotate (draw_angle)
+		love.graphics.setLineWidth (4.)
+		
+		-- arms
+		love.graphics.rectangle ("line", 
+			arm_center.x - arm_width * 0.5, 
+			arm_center.y - arm_length * 0.5,
+			arm_width, arm_length)
+		love.graphics.rectangle ("line", 
+			- arm_center.x - arm_width * 0.5, 
+			-arm_center.y + arm_length * 0.5,
+			arm_width, -arm_length)
+
+			--[[
+		love.graphics.rectangle ("line", 
+			foot_center.x - foot_width * 0.5, 
+			foot_center.y - foot_length * 0.5,
+			foot_width, foot_length)
+		love.graphics.rectangle ("line", 
+			- foot_center.x - foot_width * 0.5, 
+			-foot_center.y + foot_length * 0.5,
+			foot_width, -foot_length)
+			--]]
+
+		love.graphics.pop()
+
+
 	end
 
 	return guard
