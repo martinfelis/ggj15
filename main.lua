e453a4666a5c194e4b4c89e3826ee648e56d6980
bitowl <bitowl@googlemail.com>
2015-01-25 15:39:13 +0100

    Benutzerfreundlich

--- main.lua
+++ main.lua
@@ -45,7 +45,7 @@ states = {}
 config = { sound = true }
 audio = AudioManager:new()
 sounds = { }
-levels = { "tutorial1.svg", "tutorial2.svg", "tutorial3.svg", "tutorial4.svg", "level1.svg", "level2.svg" , "level_out.svg"}
+levels = { "tutorial1.svg", "tutorial2.svg", "tutorial3.svg", "tutorial4.svg", "level1.svg", "level_out.svg"}
 step_sounds = {}
 
 -- game design parameters
